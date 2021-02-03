1e39e0cda1a11be077038734bde9313a925b9631
igor <kametronon@mail.ru>
2021-02-03 23:05:28 +0300

    MAP, COFFEEHOUSES, COFFEESHOP +; BASKET +-

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
 import React, {useEffect, useState} from 'react';
-import {View} from '@vkontakte/vkui'
-import Main from "./components/1main/Main";
-import Basket from "./components/3basket/Basket";
-import Cash from "./components/6cash/Cash";
-import CoffeeShop from "./components/2coffeshop/CoffeeShop";
-import Editing from "./components/4editing/Editing";
-import Profile from "./components/5profile/Profile";
+import {Panel, Root, View} from '@vkontakte/vkui'
+import Basket from "./components/4basket/Basket";
 import bridge from '@vkontakte/vk-bridge';
 import './spinner.css';
+import Map from "./components/1map/Map";
+import CoffeeShopsList from "./components/2list/CoffeeShopsList";
+import CoffeeShop from "./components/3coffeshop/CoffeeShop";
 
 const PANELS = {
-    MAIN: 'MAIN',
+    MAP: 'MAP',
+    COFFEEHOUSES: 'COFFEEHOUSES',
     COFFEESHOP: 'COFFEESHOP',
+
     BASKET: 'BASKET',
     EDITING: 'EDITING',
     PROFILE: 'PROFILE',
     CASH: 'CASH',
-    ORDER: 'ORDER'
+    ORDER: 'ORDER',
+    EXPERIMENT: 'EXPERIMENT'
 }
 
 const App = () => {
-    const [panel, setPanel] = useState(PANELS.COFFEESHOP);
+    const [panel, setPanel] = useState(PANELS.BASKET);
     const [user, setUser] = useState(null);
     const [places, setPlaces] = useState([]);
     const [popout, setPopout] = useState(
         <div className="lds-css">
             <div className="lds-double-ring">
-                <div></div>
-                <div></div>
+                <div/>
+                <div/>
             </div>
         </div>
     );
-    const [tempCoffeeShop, setTempCoffeeShop] = useState({id: 0, name: 'gustogram', img: 'gust.jpg'});
+
+    // скорее всего эти вещи нужно будет переместить в дочерние компоненты
+    const [activeShop, setActiveShop] = useState(null);
+    const [shopList, setShopList] = useState([]);
+
 
     useEffect(() => {
         async function fetchData() {
@@ -40,19 +45,27 @@ const App = () => {
             setPopout(null);
         }
 
-        setPlaces(getRestData());
-        fetchData();
-    }, []);
+        async function getRestData() {
+            // будто достаю данные с бэка
+            setPlaces([
+                {id: 0, name: 'gustogram', img: 'gust.jpg'},
+                {id: 1, name: 'coffeebon', img: 'coffeebon.jpg'},
+                {id: 2, name: 'surf', img: 'surf.jpg'}
+            ]);
+        }
 
-    const getRestData = () => {
-        // get data from server
-        // plug below
-        return [
-            {id: 0, name: 'gustogram', img: 'gust.jpg'},
-            {id: 1, name: 'coffeebon', img: 'coffeebon.jpg'},
-            {id: 2, name: 'surf', img: 'surf.jpg'}
-        ];
-    }
+        getRestData();
+        fetchData();
+
+        // это для того, чтобы страница шопа отображалась, потом удалить
+        setActiveShop({id: 4, name: 'gustogram', img: 'gust.jpg'});
+
+        // это для того, чтобы корзина отображалась, потом удалить
+        setShopList([
+            {id: 4, name: 'моча', count: 14, miniImg: 'minimotcha.jpg'},
+            {id: 8, name: 'latte', count: 99, miniImg: 'latte.jpg'}
+        ]);
+    }, []);
 
     // const onCoffeeShopClick = e => {
     //     setTempCoffeeShop(e.currentTarget);
@@ -63,15 +76,35 @@ const App = () => {
     };
 
     return (
-        <View activePanel={panel} popout={popout}>
-            <Main id={PANELS.MAIN} user={user} go={go} places={places}/>
-            <CoffeeShop id={PANELS.COFFEESHOP} activeShop={tempCoffeeShop} go={go}/>
-            <Basket id={PANELS.BASKET} go={go}/>
-            <Editing id={PANELS.EDITING} go={go}/>
-            <Profile id={PANELS.PROFILE} go={go}/>
-            <Cash id={PANELS.CASH} go={go}/>
-            {/*<Order id={PANELS.ORDER} go={go}/>*/}
-        </View>
+        <Root activeView='main-panel'>
+            <View id='main-panel' activePanel={panel} popout={popout}>
+                <Panel id={PANELS.MAP}>
+                    <Map/>
+                </Panel>
+                <Panel id={PANELS.COFFEEHOUSES}>
+                    <CoffeeShopsList places={places}/>
+                </Panel>
+                <Panel id={PANELS.COFFEESHOP}>
+                    <CoffeeShop activeShop={activeShop}/>
+                </Panel>
+                <Panel id={PANELS.BASKET}>
+                    <Basket shoppingList={shopList} activeShop={activeShop}/>
+                </Panel>
+            </View>
+        </Root>
     )
+    // return (
+    //     <View activePanel={panel} popout={popout}>
+    //         <Map id={PANELS.MAP} />
+    //         <CoffeeShopsList />
+    //         {/*<CoffeeShop id={PANELS.COFFEESHOP} activeShop={tempCoffeeShop} go={go}/>*/}
+    //         {/*<Basket id={PANELS.BASKET} go={go}/>*/}
+    //         {/*<Editing id={PANELS.EDITING} go={go}/>*/}
+    //         {/*<Profile id={PANELS.PROFILE} go={go}/>*/}
+    //         {/*<Cash id={PANELS.CASH} go={go}/>*/}
+    //         {/*<Experiment id={PANELS.EXPERIMENT} go={go}/>*/}
+    //         {/*<Order id={PANELS.ORDER} go={go}/>*/}
+    //     </View>
+    // )
 }
 export default App;
